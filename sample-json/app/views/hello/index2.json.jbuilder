7d61da438d78d5cad3a16c02fe598845ea9ab27d
va034600 <va034600@gmail.com>
2015-09-10 10:27:40 +0900

    jbuilder

---
+++ sample-json/app/views/hello/index2.json.jbuilder
@@ -0,0 +1,1 @@
+json.set! :'aaa', 'bbb'
--- sample-json/config/routes.rb
+++ sample-json/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'hello/index'
+  get 'hello/index2'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
