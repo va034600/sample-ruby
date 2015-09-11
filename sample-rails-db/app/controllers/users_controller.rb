41473f8d14943dd99f015f5488e7dceee92ac92c
va034600 <va034600@gmail.com>
2015-09-11 09:59:33 +0900

    initial commit

--- sample-rails-db/app/controllers/users_controller.rb
+++ sample-rails-db/app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def index
   	@user = User.find_by(:name => "aaa")
   end
+
+  def index2
+  	@user = User.find_by(:name => params[:name])
+  end
 end
