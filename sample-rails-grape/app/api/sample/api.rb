febc0be26537c04771ac12fee3740d54962bb91f
va034600 <va034600@gmail.com>
2015-09-15 09:26:34 +0900

    grape

--- sample-rails-grape/app/api/sample/api.rb
+++ sample-rails-grape/app/api/sample/api.rb
@@ -2,9 +2,11 @@ module Sample
   class API < Grape::API
     format :json
     resource :api do
-      desc "Return sample."
       get :sample do
         "hello world"
+      end
+      get :sample2 do
+        "hello world2"
       end
     end
   end
