dbcecdc5676ea9045c169961d2d97d2306e1efab
va034600 <va034600@gmail.com>
2015-09-15 10:00:04 +0900

    initial commit

--- sample-activerecord-session/app/controllers/users_controller.rb
+++ sample-activerecord-session/app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
   def index
+  	session[:user_name] = "aaaa"
+  end
+
+  def index2
+  	@user = Hash.new
+  	@user[:name] = session[:user_name]
   end
 end
--- sample-activerecord-session/config/routes.rb
+++ sample-activerecord-session/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'users/index'
+  get 'users/index2'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
